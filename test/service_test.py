8c369c036f957d5b31f160747f1e5ab5de5803a7
Mehmet Yalçın Soruklu <50685270+myalcins@users.noreply.github.com>
2020-12-31 11:39:23 +0300

    Update service_test.py

--- test/service_test.py
+++ test/service_test.py
@@ -8,7 +8,7 @@ class TestMethod():
         self.client = None
         self.url = None
 
-    def setAuth(self):
+    def set_auth(self):
         user = baker.make(
             'User'
         )
@@ -16,30 +16,30 @@ class TestMethod():
 
     def post(self, data: Dict, authenticate=False):
         if authenticate:
-            self.client.force_authenticate(user=self.setAuth())
+            self.client.force_authenticate(user=self.set_auth())
         response = self.client.post(self.url, data, format='json')
         return response
 
     def get(self, data: Dict=None, authenticate=False):
         if authenticate:
-            self.client.force_authenticate(user=self.setAuth())
+            self.client.force_authenticate(user=self.set_auth())set_auth
         response = self.client.get(self.url, data)
         return response
 
     def patch(self, data: Dict=None, authenticate=False):
         if authenticate:
-            self.client.force_authenticate(user=self.setAuth())
+            self.client.force_authenticate(user=self.set_auth())
         response = self.client.patch(self.url, data=data)
         return response
 
     def put(self, data:Dict=None, authenticate=False):
         if authenticate:
-            self.client.force_authenticate(user=self.setAuth())
+            self.client.force_authenticate(user=self.set_auth())
         response = self.client.put(self.url, data=data)
         return response
 
     def delete(self, authenticate=False):
         if  authenticate:
-            self.client.force_authenticate(user=self.setAuth())
+            self.client.force_authenticate(user=self.set_auth())
         response = self.client.delete(self.url)
-        return response
+        return responseset_auth
